92ec52c9a1c9557b9e8c481d8c44c45dafaca37c
martialo12 <martialo12@yahoo.fr>
2024-01-31 15:26:09 +0100

    add iam permission to bucket

--- ckanext/cloudstorage/etl/ckan_manager.py
+++ ckanext/cloudstorage/etl/ckan_manager.py
@@ -23,7 +23,7 @@ class CKANManager:
     PACKAGE_SHOW_ENDPOINT = '/api/3/action/package_show?id={}'
     USER_LIST_ENDPOINT = '/api/3/action/user_list?all_fields=true'
 
-    def __init__(self, ckan_url, ckan_storage_dir, api_key=None, prefix=""):
+    def __init__(self, ckan_url, ckan_storage_dir, api_key=None, prefix="", domain=None):
         """
         Initialize the CKANManager with CKAN instance URL and API key.
 
@@ -35,6 +35,7 @@ class CKANManager:
         self.api_key = api_key
         self.ckan_storage_dir = ckan_storage_dir
         self.prefix = prefix
+        self.doamin = domain
 
     def get_request(self, url):
         """
@@ -391,6 +392,7 @@ class CKANManager:
                             if url.startswith(self.ckan_url):
                                 base_resource_dir= "{}/resources/".format(self.ckan_storage_dir)
                                 bucket_name = self.prefix + organization
+                                group_email = bucket_name + "@" + self.doamin
                                 destination_blob_name = os.path.join(
                                     'packages',
                                     package_id,
@@ -403,7 +405,7 @@ class CKANManager:
                                     if success:
                                         log.info("Downloaded {} to {}".format(file_name, file_path))
                                         # upload file to bucket
-                                        upload_to_gcp_bucket(bucket_name, destination_blob_name, file_path)
+                                        upload_to_gcp_bucket(bucket_name, destination_blob_name, file_path, group_email=group_email)
                                         self.delete_file(file_path)
                                     else:
                                         log.error("Failed to download {}".format(url))
@@ -417,7 +419,7 @@ class CKANManager:
                                     full_resource_path = os.path.join(second_path, resource)
                                     log.info("the full resource path on file system: {}".format(full_resource_path))
                                     # upload file to bucket
-                                    upload_to_gcp_bucket(bucket_name, destination_blob_name, full_resource_path)
+                                    upload_to_gcp_bucket(bucket_name, destination_blob_name, full_resource_path, group_email=group_email)
 
                             else:
                                 log.warning('Skipping external URL: {}'.format(url))
